9ace04be3c9a7424aa69e11bd0c426f884b21012
yangkequn <yangkequn@gmail.com>
2026-01-09 18:44:17 +0800

    introduce Set Get in HttpStringKey

--- http_stringkey.go
+++ http_stringkey.go
@@ -2,6 +2,7 @@ package redisdb
 
 import (
 	"fmt"
+	"time"
 
 	cmap "github.com/orcaman/concurrent-map/v2"
 )
@@ -14,14 +15,26 @@ type IHttpStringKey interface {
 	DeserializeValues(msgpacks []string) (rets []interface{}, err error)
 	TimestampFiller(in interface{}) (err error)
 
-	// HGet(field string) (interface{}, error)
-	// HGetAll() (map[string]interface{}, error)
-	// HSet(field string, val interface{}) (int64, error)
+	Set(field string, val interface{}, expiration time.Duration) (int64, error)
+	Get(field string) (interface{}, error)
 }
 
 var HttpStringKeyMap cmap.ConcurrentMap[string, IHttpStringKey] = cmap.New[IHttpStringKey]()
 
 type HttpStringKey[k comparable, v any] StringKey[k, v]
+
+func (ctx *HttpStringKey[k, v]) GetKeyType() KeyType {
+	return (*HashKey[k, v])(ctx).GetKeyType()
+}
+func (ctx *HttpStringKey[k, v]) GetUseModer() bool {
+	return (*HashKey[k, v])(ctx).GetUseModer()
+}
+func (ctx *HttpStringKey[k, v]) ValidDataKey() error {
+	return (*HashKey[k, v])(ctx).ValidDataKey()
+}
+func (ctx *HttpStringKey[k, v]) TimestampFiller(in interface{}) (err error) {
+	return (*HashKey[k, v])(ctx).TimestampFiller(in)
+}
 
 func (ctx *HttpStringKey[k, v]) DeserializeValue(msgpack []byte) (rets interface{}, err error) {
 	return ctx.DeserializeToValue(msgpack)
@@ -37,4 +50,25 @@ func GetHttpStringKey(Key string, rdsName string) (IHttpStringKey, error) {
 		return nil, fmt.Errorf("key schema not found")
 	}
 	return ikey, nil
+}
+func (ctx *HttpStringKey[k, v]) Set(field string, val interface{}, expiration time.Duration) (err error) {
+	var key k
+	key, err = ctx.toKey([]byte(field))
+	if err != nil {
+		return err
+	}
+	var _v v
+	_v, ok := val.(v)
+	if !ok {
+		return fmt.Errorf("value type assertion failed")
+	}
+	return (*StringKey[k, v])(ctx).Set(key, _v, expiration)
+}
+func (ctx *HttpStringKey[k, v]) Get(field string) (val interface{}, err error) {
+	var key k
+	key, err = ctx.toKey([]byte(field))
+	if err != nil {
+		return nil, err
+	}
+	return (*StringKey[k, v])(ctx).Get(key)
 }
