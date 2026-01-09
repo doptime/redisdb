920f6cfcbe3464b75d223dcc5a57a491bd2dad6c
yangkequn <yangkequn@gmail.com>
2026-01-09 15:48:42 +0800

    add IHttpKey

---
+++ http_key.go
@@ -0,0 +1,10 @@
+package redisdb
+
+type IHttpKey interface {
+	GetKeyType() KeyType
+	GetUseModer() bool
+	ValidDataKey() error
+	DeserializeValue(msgpack []byte) (rets interface{}, err error)
+	DeserializeValues(msgpacks []string) (rets []interface{}, err error)
+	TimestampFiller(in interface{}) (err error)
+}
